971e8a6a117e052876e93323588ac511d2d3723f
Stoneage <kayaeryilmaz18@gmail.com>
2024-02-01 03:10:42 +0300

    Json file eklentisi

---
+++ lib/widgets/List.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localdatabase/models/cookmodels.dart';
+
+class ListBuilder extends StatefulWidget {
+  const ListBuilder({
+    super.key,
+    required this.selectedList,
+    required this.isSelectionMode,
+    required this.onSelectionChange,
+    required this.data
+    
+  });
+
+  final bool isSelectionMode;
+  final List<bool> selectedList;
+  final ValueChanged<bool>? onSelectionChange;
+  final List<cokkies> data;
+
+
+  @override
+  State<ListBuilder> createState() => _ListBuilderState();
+}
+
+class _ListBuilderState extends State<ListBuilder> {
+  void _toggle(int index) {
+    if (widget.isSelectionMode) {
+      setState(() {
+        widget.selectedList[index] = !widget.selectedList[index];
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: widget.data.length,
+        itemBuilder: (_, int index) {
+          return ListTile(
+            leading: const CircleAvatar(backgroundColor:  Color.fromARGB(255, 178, 190, 201)),
+              onTap: () => _toggle(index),
+              onLongPress: () {
+                if (!widget.isSelectionMode) {
+                  setState(() {
+                    widget.selectedList[index] = true;
+                  });
+                  widget.onSelectionChange!(true);
+                }
+              },
+              trailing: widget.isSelectionMode
+                  ? Checkbox(
+                      value: widget.selectedList[index],
+                      onChanged: (bool? x) => _toggle(index),
+                    )
+                  : const SizedBox.shrink(),
+              title: Text(widget.data[index].getname));
+        
+        });
+  }
+}
